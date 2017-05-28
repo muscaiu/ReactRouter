d5fcd8bbe73e2f334850e49e0924a62a4fa136b9
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-28 14:02:51 +0300

    adding first component to route

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Layout extends React.Component {
   render() {
     return (
-    <h1>KillerNews.net</h1>
+      <h1>KillerNews.net</h1>
     );
   }
 }
