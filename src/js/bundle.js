2b38fdee7a4241f604f04e4efc66fe81b95c78d5
Cristian Muscalu <muscaiu@yahoo.com>
2017-06-04 20:30:57 +0300

    adding basic redux

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { createStore } from 'redux'
 
 import Layout from "./pages/Layout";
 import Archives from "./pages/Archives";
@@ -9,6 +10,23 @@ import Featured from "./pages/Featured";
 import Todos from "./pages/Todos";
 
 const app = document.getElementById('app');
+//redux
+const reducer = function (state, action) {
+    if (action.type === 'INC') {
+        return state + action.payload
+    }
+    if (action.type === 'DEC') {
+        return state - action.payload
+    }
+    return state
+}
+const store = createStore(reducer, 0)
+store.subscribe(() => {
+    console.log('store changed', store.getState())
+})
+store.dispatch({ type: 'INC', payload: 5 })
+store.dispatch({ type: 'INC', payload: 10 })
+store.dispatch({ type: 'DEC', payload: 20 })
 
 ReactDOM.render(
     <Router history={hashHistory}>
